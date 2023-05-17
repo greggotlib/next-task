26b2c7cb5a099716197f84bd739e5e5a6f71732c
gregory gotlib <gregory.gotlib@gmail.com>
2023-05-17 16:53:44 +0300

    Remove test call

--- src/api/moviesApi.ts
+++ src/api/moviesApi.ts
@@ -6,7 +6,6 @@ const serverUrl = 'http://localhost:8000'
 export const fetchMovies = async (): Promise<Movie[]> => {
   try {
     const response = await axios.get(`${serverUrl}/movies`)
-    fetchMovie('207856')
     if (response.status !== 200) {
       throw new Error(
         `Failed to fetch movies, the server return with the status of ${response.status}`
